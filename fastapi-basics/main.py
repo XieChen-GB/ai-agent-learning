8e84059b3639d7fcb2c408f74e089423b5927947
Xie Chen <xieche@gmail.com>
2026-06-13 11:37:56 +0900

    fastapi-basics: module3 POST endpoint with Pydantic

--- fastapi-basics/main.py
+++ fastapi-basics/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 app = FastAPI()
+
+class ChatRequest(BaseModel):
+    message: str
+    session_id: str
 
 @app.get("/agent/status")
 def get_status():
@@ -8,3 +14,7 @@ def get_status():
 @app.get("/agent/info")
 def get_info():
     return {"name": "謝晨", "model": "qwen2.5:7b"}
+
+@app.post("/agent/chat")
+def post_char(chartrequest: ChatRequest):
+    return {"reply": f"收到:{chartrequest.message}", "session": chartrequest.session_id} 
